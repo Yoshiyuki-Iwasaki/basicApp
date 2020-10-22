0ec62a8b0ee2ec829605eabcaa3215c58509b687
Yoshiyuki Iwasaki <yoshiyuki_iwasaki@commude.co.jp>
2020-10-23 02:14:26 +0900

    [add] ボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
 
 const EffectFunc = () => {
-  const [count, setCount] = useState(0)
+  // countの初期値として、1~10までのランダムな数値を生成
+  const intialState = Math.floor(Math.random() * 10) + 1
+  // count という名前の state 変数を宣言、初期値 intialState をセット
+  const [count, setCount] = useState(intialState);
   useEffect(() => {
     document.title =`${count}回クリックされました`
   })
@@ -12,12 +15,9 @@ const EffectFunc = () => {
     <div className="App">
       <p>{`${count}回クリックされました`}</p>
       <ButtonGroup color="primary" aria-label="outlined primary button group">
-        <Button onClick={()=>setCount((prev) => prev + 1)}>
-          ボタン
-        </Button>
-        <Button onClick={()=>setCount(0)}>
-          リセット
-        </Button>
+        <Button onClick={()=>setCount((prev) => prev + 1)}>ボタン</Button>
+        <Button onClick={()=>setCount(0)}>リセット</Button>
+        <Button onClick={() => setCount(intialState)}>最初の数値に戻す</Button>
       </ButtonGroup>
     </div>
   )
